2c6ad7dd03ee3121f3263dbb9a013f390eed7ca6
Minh Anh <anhminhngo9@gmail.com>
2024-09-22 01:52:35 +0700

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Auth from "./Log";
+
+const Home = () => {
+  return (
+    <div>
+      <Auth />
+      <h1>hi</h1>
+    </div>
+  );
+};
+
+export default Home;
