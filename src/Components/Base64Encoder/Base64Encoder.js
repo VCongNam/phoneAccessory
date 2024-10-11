3bfb91039d7238e41afdcd7a43b2e118db9e3f8f
hadat <dathmhe170960@fpt.edu.vn>
2024-10-11 08:09:52 +0700

    update base64 function

--- src/Components/Base64Encoder/Base64Encoder.js
+++ src/Components/Base64Encoder/Base64Encoder.js
@@ -1,7 +1,20 @@
-function base64Encoder(value) {
+
+
+  function encoder64(value) {
     const encoder = new TextEncoder('utf-8');
     const encodedValue = encoder.encode(value);
     return btoa(String.fromCharCode(...encodedValue));
   }
 
-  export default base64Encoder;
+  function decoder64(value) {
+    try {
+      const decoder = new TextDecoder('utf-8');
+      const decodedValue = decoder.decode(atob(value));
+      return decodedValue;
+    } catch (error) {
+      console.error('Failed to decode string:', error);
+      return null;
+    }
+  }
+  
+  export { encoder64, decoder64 };
