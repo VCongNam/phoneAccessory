71136c2a5124df0c5b071cf04624c98dacdcab42
hadat <dathmhe170960@fpt.edu.vn>
2024-10-11 08:51:06 +0700

    update Base64 logic

--- src/Components/Base64Encoder/Base64Encoder.js
+++ src/Components/Base64Encoder/Base64Encoder.js
@@ -1,20 +1,24 @@
-
-
-  function encoder64(value) {
-    const encoder = new TextEncoder('utf-8');
-    const encodedValue = encoder.encode(value);
-    return btoa(String.fromCharCode(...encodedValue));
+function encoder64(value) {
+  try {
+    // Convert the value to a base64 encoded string
+    return btoa(unescape(encodeURIComponent(value)));
+  } catch (error) {
+    console.error('Failed to encode string:', error);
+    return null;
   }
+}
 
-  function decoder64(value) {
-    try {
-      const decoder = new TextDecoder('utf-8');
-      const decodedValue = decoder.decode(atob(value));
-      return decodedValue;
-    } catch (error) {
-      console.error('Failed to decode string:', error);
-      return null;
+function decoder64(value) {
+  try {
+    // Validate that the input string is base64
+    if (!value || !/^[A-Za-z0-9+/=]+$/.test(value)) {
+      throw new Error('Invalid base64 string');
     }
+    // Decode the base64 string back to a normal string
+    return decodeURIComponent(escape(atob(value)));
+  } catch (error) {
+    console.error('Failed to decode string:', error);
+    return null;
   }
-  
-  export { encoder64, decoder64 };
+}
+export { encoder64, decoder64 };
