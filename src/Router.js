312bd8cf8156a6c157e4921b0c60829b5bfa0852
hadat <dathmhe170960@fpt.edu.vn>
2024-10-11 08:50:57 +0700

    implement PrivateRoutes into Router

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AppHeader from './Components/Header/Header';
-import AppFooter from './Components/Footer/Footer';
 import Home from './Pages/Home';
 import Auth from './Pages/Log';
+import PrivateRoutes from './PrivateRoutes';
 import ProductList from './Pages/ProductList';
 import ProductDetail from './Pages/ProductDetail';
 import DashboardPage from './Pages/AdminDash';
@@ -16,8 +15,11 @@ const AppRouter = () => {
                 {/* Define Routes */}
                 <Routes>
                     {/* Route for Home page */}
-                    <Route path="/" element={<Home/>} />
-                    <Route path="/dashboard" element={<DashboardPage />} />
+                    <Route element={<PrivateRoutes />}>
+                        <Route path="/dashboard" element={<DashboardPage />} />
+                    </Route>
+                    <Route path="/" element={<Home />} exact />
+                    {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
                     <Route path="/productlist" element={<ProductList />} />
                     <Route path="/productdetail/:id" element={<ProductDetail />} />
                     <Route path="/login" element={<Auth />} />
