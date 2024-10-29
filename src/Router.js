a96b689b79df10d114204c89cf239849a76bec2d
hadat <dathmhe170960@fpt.edu.vn>
2024-10-29 10:17:06 +0700

    Update

--- src/Router.js
+++ src/Router.js
@@ -18,7 +18,7 @@ const AppRouter = () => {
                 <Routes>
                     <Route element={<PrivateRoutes />}>
                         <Route path="/dashboard" element={<DashboardPage />} />
-                        <Route path="/SellerDashboard" element={<SellerDashborard />} />
+                        <Route path="/SellerDashboard" element={<SellerDashborard />} /> 
                     </Route>
                     <Route path="/adminlogin" element={<AdminAuth />}/>
                     <Route path="/sellerlogin" element={<SellerAuth />}/>
